08b4cca64f3ed20d4bd60f52fb274097d85ac756
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-22 14:29:59 +0900

    Import pyqt-fitting-graphics-view and make FittingGraphicsView class parent class of ViewerGraphicsView class

--- pyqt_viewer_widget/viewerGraphicsView.py
+++ pyqt_viewer_widget/viewerGraphicsView.py
@@ -1,44 +1,27 @@
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsItem
+from PyQt5.QtWidgets import QGraphicsItem
 from PyQt5.QtCore import Qt, pyqtSignal, QPoint, QRect
+from pyqt_fitting_graphics_view.fittingGraphicsView import FittingGraphicsView
 
 
-class ViewerGraphicsView(QGraphicsView):
+class ViewerGraphicsView(FittingGraphicsView):
     photoClicked = pyqtSignal(QPoint)
     rectChanged = pyqtSignal(QRect)
 
     def __init__(self):
         super().__init__()
         self.__filenames = []
-        self.__initUi()
-
-    def __initUi(self):
-        self.__p = 0
-        self.__scene = 0
-        self.__graphicItem = 0
 
     def setFilenames(self, filenames):
         self.__filenames = filenames
 
     def setIndex(self, index):
-        self.setFile(self.__filenames[index])
-
-    def setPixmap(self, p):
-        self.__setPixmap(p)
-
-    def setFile(self, filename):
-        self.__p = QPixmap(filename)
-        self.__setPixmap(self.__p)
+        self.setFilename(self.__filenames[index])
 
     def __setPixmap(self, p):
         self.__p = p
-        self.__scene = QGraphicsScene()
-        self.__graphicItem = self.__scene.addPixmap(self.__p)
-        self.setScene(self.__scene)
-        # fit in view literally
-        self.fitInView(self.__graphicItem, Qt.KeepAspectRatio)
-        self.show()
+        self._item = self._scene.addPixmap(self._p)
+        self.setScene(self._scene)
 
     def resizeEvent(self, e):
-        if isinstance(self.__graphicItem, QGraphicsItem):
-            self.fitInView(self.__graphicItem, Qt.KeepAspectRatio)
+        if isinstance(self._item, QGraphicsItem):
+            self.fitInView(self._item, Qt.KeepAspectRatio)
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     install_requires=[
         'PyQt5>=5.8',
         'Pillow>=9.0.0',
-        'pyqt-toast @ git+https://git@github.com/yjg30737/pyqt-toast.git@main'
+        'pyqt-toast @ git+https://git@github.com/yjg30737/pyqt-toast.git@main',
+        'pyqt-fitting-graphics-view @ git+https://git@github.com/yjg30737/pyqt-fitting-graphics-view.git@main'
     ]
 )
