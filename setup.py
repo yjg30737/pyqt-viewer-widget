437ac798f498ebd40a78422dce974a71be0e8062
Jung Gyu Yoon <yjg30737@gmail.com>
2022-02-28 12:57:53 +0900

    Update version (0.5.0 -> 0.6.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-viewer-widget',
-    version='0.5.0',
+    version='0.6.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
