d0504daffd1aff87a4d1163ee99fc1411e1ed62d
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-03 14:22:12 +0900

    Add new feature (remove some files from viewer)

--- pyqt_viewer_widget/viewerWidget.py
+++ pyqt_viewer_widget/viewerWidget.py
@@ -244,3 +244,24 @@ class ViewerWidget(QWidget):
 
     def getLastPageToast(self):
         return self.__lastPageToast
+
+    def removeSomeFilesFromViewer(self, filenames_to_remove: list):
+        cur_idx = self.getCurrentIndex()
+        filenames = self.getFilenames()
+        maintain_cur_idx_if_cur_idx_file_still_remain = filenames[cur_idx] in filenames_to_remove
+
+        for filename in filenames_to_remove:
+            filenames.remove(filename)
+
+        if len(filenames) == 0:
+            pass
+        elif len(filenames) > cur_idx:
+            if maintain_cur_idx_if_cur_idx_file_still_remain:
+                cur_filename = filenames[cur_idx]
+                self.setFilenames(filenames, cur_filename)
+            else:
+                cur_filename = filenames[cur_idx - (len(filenames)-len(filenames_to_remove))]
+                self.setFilenames(filenames, cur_filename)
+        elif len(filenames) <= cur_idx:
+            cur_filename = filenames[-1]
+            self.setFilenames(filenames, cur_filename)
