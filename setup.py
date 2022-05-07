57d0adaf1d8582b57a2c909ce1c94f838fc9b258
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-07 18:33:43 +0900

    Update setup.py for pypi description, bump version (0.0.12 -> 0.0.14)

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-viewer-widget',
-    version='0.0.12',
+    version='0.0.14',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt viewer widget which helps you make viewer application easily',
     url='https://github.com/yjg30737/pyqt-viewer-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-toast>=0.0.1',
